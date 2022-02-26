ccc8a8d8e8352e8432821ca3cd927a3289fa5c48
Olga Eidelman <olya451208@gmail.com>
2022-02-26 19:02:44 +0200

    initial commit

---
+++ js/cart.js
@@ -0,0 +1,17 @@
+const cart = function () {
+
+    const cartBtn = document.querySelector('.button-cart');
+    const cart = document.getElementById('modal-cart');
+    const closeBtn = cart.querySelector('.modal-close');
+
+    console.log(cart);
+    cartBtn.addEventListener('click', function () {
+        cart.style.display = 'flex'
+    })
+
+    closeBtn.addEventListener('click', function () {
+        cart.style.display = ''
+    })
+
+}
+cart();
